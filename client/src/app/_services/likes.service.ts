1c6cdbebd28f0ec438b68408a89e1bb7009d5d13
Wally <52685157+wallywhatever@users.noreply.github.com>
2024-09-11 18:10:46 -0400

    implemented liked lists

--- client/src/app/_services/likes.service.ts
+++ client/src/app/_services/likes.service.ts
@@ -1,25 +1,28 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Member } from '../_models/member';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LikesService {
   baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
-  likeIds = signal<number[]>([])
+  likeIds = signal<number[]>([]);
 
   toggleLike(targetId: number) {
-    return this.http.post(`${this.baseUrl}likes/${targetId}`, {})
+    return this.http.post(`${this.baseUrl}likes/${targetId}`, {});
   }
 
   getLikes(predicate: string) {
-    return this.http.get(`${this.baseUrl}likes?predicate=${predicate}`)
+    return this.http.get<Member[]>(
+      `${this.baseUrl}likes?predicate=${predicate}`
+    );
   }
 
   getLikeIds() {
     return this.http.get<number[]>(`${this.baseUrl}likes/list`).subscribe({
-      next: ids => this.likeIds.set(ids)
-    })
+      next: (ids) => this.likeIds.set(ids),
+    });
   }
 }
