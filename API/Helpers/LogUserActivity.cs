18a7dee97e892e709a35cfde933ab002635b65a6
Wally <52685157+wallywhatever@users.noreply.github.com>
2024-09-11 12:35:16 -0400

    LogUserActivity optimized to reduce backend load

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -11,13 +11,13 @@ public class LogUserActivity : IAsyncActionFilter
     {
         var resultContext = await next();
 
-        if(context.HttpContext.User.Identity?.IsAuthenticated != true) return;
+        if (context.HttpContext.User.Identity?.IsAuthenticated != true) return;
 
-        var username = resultContext.HttpContext.User.GetUserName();
+        var userId = resultContext.HttpContext.User.GetUserId();
 
         var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-        var user = await repo.GetUserByUsernameAsync(username);
-        if (user == null ) return;
+        var user = await repo.GetUserByIdAsync(userId);
+        if (user == null) return;
         user.LastActive = DateTime.UtcNow;
         await repo.SaveAllAsync();
     }
