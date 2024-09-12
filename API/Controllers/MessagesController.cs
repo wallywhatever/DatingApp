f5933adc43040ac8b120b41989fb0990cb43b1a7
Wally <52685157+wallywhatever@users.noreply.github.com>
2024-09-12 10:33:26 -0400

    sendmessage api functionality in messagescontroller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+using System;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class MessagesController(IMessageRepository messageRepository, IUserRepository userRepository,
+    IMapper mapper) : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+    {
+        var username = User.GetUserName();
+
+        if (username == createMessageDto.RecipientUsername.ToLower())
+        {
+            return BadRequest("You can't message yourself");
+        }
+
+        var sender = await userRepository.GetUserByUsernameAsync(username);
+        var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+        if (recipient == null || sender == null) return BadRequest("Cannot send message at this time");
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+
+        messageRepository.AddMessage(message);
+
+        if (await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
+
+        return BadRequest("Failed to save message");
+    }
+}
