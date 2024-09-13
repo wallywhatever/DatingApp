485a552451c45e7242aa75e7d76894a2b4855aff
Wally <52685157+wallywhatever@users.noreply.github.com>
2024-09-12 21:48:39 -0400

    used a route resolver to get the member ahead of page load so that i could be used to load the messages and automatically switch to that tab on clicking a message or the envelope on memberlist

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,38 +3,71 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
-import { GalleryItem, GalleryModule, ImageItem} from 'ng-gallery'
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
-import { MemberMessagesComponent } from "../member-messages/member-messages.component";
+import { MemberMessagesComponent } from '../member-messages/member-messages.component';
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent],
+  imports: [
+    TabsModule,
+    GalleryModule,
+    TimeagoModule,
+    DatePipe,
+    MemberMessagesComponent,
+  ],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css',
 })
 
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs?: TabsetComponent
+  @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent;
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   private messageService = inject(MessageService);
-  member?: Member;
+  member: Member = {} as Member;
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: (data) => {
+        this.member = data['member'];
+      },
+    });
+
+    this.route.queryParams.subscribe({
+      next: (params) => {
+        params['tab'] && this.selectTab(params['tab']);
+        this.member &&
+          this.member.photos.map((p) => {
+            this.images.push(new ImageItem({ src: p.url, thumb: p.url }));
+          });
+      },
+    });
+  }
+
+  selectTab(heading: string) {
+    if (this.memberTabs) {
+      const messageTab = this.memberTabs.tabs.find(
+        (x) => x.heading === heading
+      );
+      if (messageTab) messageTab.active = true;
+    }
   }
 
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
-    if(this.activeTab.heading === "Messages" && this.messages.length === 0 && this.member){
+    if (
+      this.activeTab.heading === 'Messages' &&
+      this.messages.length === 0 &&
+      this.member
+    ) {
       this.messageService.getMessageThread(this.member.username).subscribe({
         next: (messages) => (this.messages = messages),
       });
@@ -47,9 +80,9 @@ export class MemberDetailComponent implements OnInit {
     this.memberService.getMember(username).subscribe({
       next: (member) => {
         this.member = member;
-        member.photos.map(p => {
-          this.images.push(new ImageItem({src: p.url, thumb: p.url}))
-        })
+        member.photos.map((p) => {
+          this.images.push(new ImageItem({ src: p.url, thumb: p.url }));
+        });
       },
     });
   }
