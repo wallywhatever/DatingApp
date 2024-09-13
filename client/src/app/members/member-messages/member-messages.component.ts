ab5ebc3a7522beb1677e99453d41c8c4fc53fdb3
Wally <52685157+wallywhatever@users.noreply.github.com>
2024-09-12 21:59:47 -0400

    implemented basic message sending on client

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,27 +1,31 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
-  imports: [TimeagoModule],
+  imports: [TimeagoModule, FormsModule],
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css',
 })
-export class MemberMessagesComponent implements OnInit {
+export class MemberMessagesComponent{
+  @ViewChild('messageForm') messageForm?: NgForm;
   private messageService = inject(MessageService);
   username = input.required<string>();
-  messages = input.required<Message[]>();
+  messages = input.required<Message[]>(); 
+  messageContent = '';
+  updateMessages = output<Message>()
 
-  ngOnInit(): void {
-    // this.loadMessages();
+  sendMessage() {
+    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
+      next: message => {
+        this.updateMessages.emit(message);
+        this.messageForm?.reset();
+      }
+    })
   }
 
-  // loadMessages() {
-  //   this.messageService.getMessageThread(this.username()).subscribe({
-  //     next: (messages) => (this.messages = messages),
-  //   });
-  // }
 }
