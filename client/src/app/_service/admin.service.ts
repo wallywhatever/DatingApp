d894f4cc4477437818286332bab1723b57497244
Wally <52685157+wallywhatever@users.noreply.github.com>
2024-09-13 23:20:14 -0400

    client user management admin panel implemented

--- client/src/app/_service/admin.service.ts
+++ client/src/app/_service/admin.service.ts
@@ -4,13 +4,20 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
-baseUrl = environment.apiUrl;
-private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
 
-getUserWithRoles() {
-  return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles')
-}
+  getUserWithRoles() {
+    return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
+  }
+
+  updateUserRoles(username: string, roles: string[]) {
+    return this.http.post<string[]>(
+      this.baseUrl + 'admin/edit-roles/' + username + '?roles=' + roles,
+      {}
+    );
+  }
 }
--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -15,23 +15,38 @@ export class UserManagementComponent implements OnInit {
   private adminService = inject(AdminService);
   private modalService = inject(BsModalService);
   users: User[] = [];
-  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
-
+  bsModalRef: BsModalRef<RolesModalComponent> =
+    new BsModalRef<RolesModalComponent>();
 
   ngOnInit(): void {
     this.getUsersWithRoles();
-    console.log('test');
   }
 
-  openRolesModal() {
+  openRolesModal(user: User) {
     const initialState: ModalOptions = {
       class: 'modal-lg',
       initialState: {
         title: 'User roles',
-        list: ['Admin', 'Moderator', 'Member']
-      }
-    }
+        username: user.username,
+        selectedRoles: [...user.roles],
+        availableRoles: ['Admin', 'Moderator', 'Member'],
+        users: this.users,
+        rolesUpdated: false,
+      },
+    };
     this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    this.bsModalRef.onHide?.subscribe({
+      next: () => {
+        if (this.bsModalRef.content && this.bsModalRef.content.rolesUpdated) {
+          const selectedRoles = this.bsModalRef.content.selectedRoles;
+          this.adminService
+            .updateUserRoles(user.username, selectedRoles)
+            .subscribe({
+              next: (roles) => (user.roles = roles),
+            });
+        }
+      },
+    });
   }
 
   getUsersWithRoles() {
